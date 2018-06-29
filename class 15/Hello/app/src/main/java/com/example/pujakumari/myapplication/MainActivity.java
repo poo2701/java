d449eb6a1554684f8812f53784245a8f87f1c4ca
poo2701 <purnimaraj27@gmail.com>
2018-06-29 12:32:39 +0530

    android

---
+++ class 15/Hello/app/src/main/java/com/example/pujakumari/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pujakumari.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private view v;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void function_Click(view V){
+
+        Toast
+    }
+}
